8610d2af97b5316de9d35f56fac4d366234dc685
Hanna Hellsen <hanna.hellsen@gmail.com>
2021-12-11 17:21:47 +0100

    hej

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const countdown = () => {
     const countDate = new Date("May 17, 2021 00:00:00").getTime();
-    console.log(countDate);
+    const now = new Date().getTime();
+    const gap = countDate -now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = gap / day;
+    console.log(textDay);
 };
 
 countdown();
